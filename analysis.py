7ab448527ea5bbacb932048fd131d8fbae9389ff
Gaudenz Halter <gaudenz.halter@live.com>
2023-04-05 18:14:31 +0200

    .wip(supply-chain): added some basic vis

--- analysis.py
+++ analysis.py
@@ -3,6 +3,84 @@ from climada_petals.engine import SupplyChain
 from climada.entity import ImpfTropCyclone, ImpactFuncSet
 from climada.engine.impact_calc import ImpactCalc
 
+import numpy as np
+
+from bokeh.plotting import figure, show, output_file
+from bokeh.layouts import layout
+from bokeh.models import ColumnDataSource
+from bokeh.transform import linear_cmap
+
+
+def plot_tot_prod_impt_eai(data):
+    """Plot indirect impact"""
+    data = [(k, v) for k, v in sorted(data.items())]
+    data = sorted(data, key=lambda x: x[1], reverse=True)
+
+    sectors = [k[0][:50] for k in data]
+    values = [k[1] for k in data]
+
+    source = ColumnDataSource(
+        dict(
+            sectors = sectors,
+            impact = values
+        )
+    )
+    p = figure(
+        x_range=sectors, height=700, width=1400, title="Expected total production impact for Switzerland",
+    )
+
+    p.vbar(x="sectors", top="impact", width=0.9, source=source)
+
+    p.y_range.start = 0
+    p.xaxis.major_label_orientation = np.pi/4
+
+    return p
+
+def plot_source_matrix(data, x_range=None):
+
+    #  Currently only one country
+    data = [(k, v, "USA") for k, v in sorted(data.items())]
+    data = sorted(data, key=lambda x: x[1], reverse=True)
+
+    sectors = [k[0][:50] for k in data]
+    values = [k[1] for k in data]
+    countries = [k[2] for k in data]
+
+    source = ColumnDataSource(
+        dict(
+            sectors = sectors,
+            impact = values,
+            country = countries
+        )
+    )
+
+    cmap = linear_cmap(
+        "impact", palette="Inferno256",
+        low=min(values), high=max(values)
+    )
+    p = figure(
+        x_range=x_range, y_range=list(set(countries)), height=100, width=1400, title="Expected total production impact for Switzerland",
+    )
+    p.circle(x='sectors', y='country', color=cmap, size=10, source=source, )
+    p.xaxis.major_label_text_color = None
+    return p
+
+
+
+def create_supply_chain_vis(supchain: SupplyChain):
+    """Create supply chain visualization"""
+    output_file(filename="custom_filename.html", title="Static HTML file")
+
+    data = supchain.tot_prod_impt_eai.loc[('CHE', slice(None))]
+    p1 = plot_tot_prod_impt_eai(data)
+    p2 = plot_source_matrix(data, x_range=p1.x_range)
+    show(layout([p2, p1]))
+
+
+
+
+
+
 def main():
     client = Client()
 
@@ -14,7 +92,7 @@ def main():
 
     tc_usa = client.get_hazard('tropical_cyclone', properties={'country_iso3alpha':'USA', 'climate_scenario':'historical'})
 
-    # Define impact function
+    # Define direct_impact function
     impf_tc = ImpfTropCyclone.from_emanuel_usa()
     impf_set = ImpactFuncSet()
     impf_set.append(impf_tc)
@@ -31,7 +109,7 @@ def main():
 
     supchain = SupplyChain.from_mriot(mriot_type='WIOD16', mriot_year=2011)
 
-    # Assign exposure and stock impact to MRIOT country-sector
+    # Assign exposure and stock direct_impact to MRIOT country-sector
     
     # (Service sector)
     impacted_secs = supchain.mriot.get_sectors()[range(26,56)].tolist()
@@ -46,7 +124,8 @@ def main():
     # Calculate total production loss
     supchain.calc_total_production_impacts(direct_impact_usa)
 
-
+    # Some basic Vis
+    create_supply_chain_vis(supchain)
     # Everything in this section equivalent to
     #    supchain.calc_production_impacts(direct_impact_usa, exp_usa, impacted_secs=impacted_secs, io_approach='ghosh')
 
