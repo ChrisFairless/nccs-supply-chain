78edbfbf7c0efc0f2d4a6b6f450a4f93e853d588
Chris Fairless <chrisfairless@hotmail.com>
2023-04-19 15:49:05 +0200

    WIP

--- direct.py
+++ direct.py
@@ -35,6 +35,87 @@ def nccs_direct_impacts_simple(haz_type, sector, country, scenario, ref_year):
     return ImpactCalc(exp, impf_set, haz).impact(save_mat=True)
 
 
+def nccs_direct_impacts(hazard_list, sector_list, country_list, scenario):
+    client = Client()
+
+    country_iso3alpha_list = [pycountry.countries.get(name=country).alpha_3 for country in country_list]
+
+    # all combinations of hazard, exposure and country
+    analyses_hazards = [
+        {
+            "hazard": hazard,
+            "country": country,
+            "reg_id": i_country,
+            "haz": client.get_hazard(hazard, properties={'country_iso3alpha': country, 'climate_scenario': scenario}),
+        } 
+        for i_country, country in enumerate(country_iso3alpha_list) for hazard in hazard_list
+    ]
+
+    # all exposures
+    analyses_exposures = [
+        {
+            "sector": sector,
+            "country": country,
+            "reg_id": i_country,
+            "exp": get_sector_exposure(sector, country),
+        } 
+        for i_country, country in enumerate(country_iso3alpha_list) for sector in sector_list
+    ]
+
+    for item in analyses_exposures:
+        # Set region ID
+        item['exp'].gdf['reg_id'] = item['reg_id']
+
+    # generate impact functions
+    analyses_impfs = [
+        {
+            "sector": sector,
+            "country": country,
+            "reg_id": i_country,
+            "impf_set": get_sector_impf_set(hazard, sector, country)
+        }
+        for i_country, country in enumerate(country_iso3alpha_list) for sector in sector_list for hazard in hazard_list
+    ]
+
+    # Identify which hazards have matching centroids
+    analyses_hazards['centroid_set_id'] = [
+        np.which([
+            np.all_equal(analyses_hazards[i, 'haz'].centroids.lat, analyses_hazards[j, 'haz'].centroids.lat)
+            for j in range(i)
+        ])[0]
+        for i in range(analyses_hazards.shape[0])
+    ]
+
+    # And assign centroids to exposures accordingly
+    for 
+
+    analyses_hazards = pd.DataFrame(analyses_hazards)
+    analyses_exposures = pd.DataFrame(analyses_exposures)
+    analyses_impfs = pd.DataFrame(analyses_impfs).drop_duplicates()
+    analyses_impfs['impf_id'] = np.arange(analyses_impfs.shape[0])
+
+    analyses_all = analyses_exposures.join(analyses_hazards, on=['country', 'reg_id']).join(analyses_impfs, on=['country', 'reg_id', 'sector'])
+
+    analyses_all['imp'] = [ImpactCalc(row['exp'], row['impf_set'], row['haz']) for row in tqdm(analyses_all.itertuples())]
+
+
+    aggregation_dimensions = set()
+    if not save_by_country:
+        analyses.aggregate()
+    if not save_by_hazard:
+        aggregation_dimensions.add('hazard')
+    if not save_by_sector:
+        aggregation_dimensions.add('sector')
+       
+
+
+
+    exp_all = Exposures.concat(exp_list)
+
+    # Calculate direct impacts to the USA due to TC
+    imp_calc = ImpactCalc(exp_usa, impf_set, tc_usa)
+    direct_impact_usa = imp_calc.impact()
+
 
 def get_sector_exposure(sector, country):
     if sector == 'service':
