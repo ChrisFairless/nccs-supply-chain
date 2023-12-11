e39ebf5da710246f84ad6cae029493dd8a1f13e7
Gaudenz Halter <gaudenz.halter@live.com>
2023-12-11 09:53:28 +0100

    .wip(config): added basic structure for config

--- analysis.py
+++ analysis.py
@@ -37,7 +37,8 @@ Tropical Cyclones:
     "synthetic", could also use "observed"
 
 River Flood: 
-    'year_range': ['2010_2030','2030_2050','2050_2070','2070_2090','historical'], # officialy in API 'historical' would be '1980_2000'
+    'year_range': ['2010_2030','2030_2050','2050_2070','2070_2090','historical'], # officialy in API 'historical' 
+    would be '1980_2000'
     'climate_scenario': ['rcp26', 'rcp85', 'None', 'rcp60'] # officialy in API, 'None' would be 'historical'
 
     combination that are allowed:
@@ -73,56 +74,82 @@ Wildfire:
 Agriculture: Relative Cropyield
     hazard: 'relative_crop_yield'
     sector: 'agriculture'
-    scenario = 'climate_scenario': ['historical', 'rcp60'] & ref_year = 'year_range': ['1971_2001', '1980_2012', '2006_2099', '1976_2005']    
+    scenario = 'climate_scenario': ['historical', 'rcp60'] & ref_year = 'year_range': ['1971_2001', '1980_2012', 
+    '2006_2099', '1976_2005']    
     hazard_list = ['relative_crop_yield']  # ['tropical_cyclone', 'river_flood', 'storm_europe', 'relative_crop_yield']
 """
 
+# Check for country names with this website:
+# https://github.com/flyingcircusio/pycountry/blob/main/src/pycountry/databases/iso3166-1.json
+#
+# country_list = ['United States']
+# ALL_COUNTRIES = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina',
+#                  'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados',
+#                  'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia, Plurinational State of',
+#                  'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso',
+#                  'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon',
+#                  'Canada', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo',
+#                  'Congo, The Democratic Republic of the', 'Costa Rica', 'Croatia', "Côte d'Ivoire",
+#                  'Cuba', 'Cyprus', 'Czechia', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic',
+#                  'Timor-Leste', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea',
+#                  'Estonia', 'Eswatini', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia',
+#                  'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti',
+#                  'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq',
+#                  'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati',
+#                  "Korea, Democratic People's Republic of",
+#                  'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia',
+#                  'Lebanon',
+#                  'Lesotho', 'Liberia',
+#                  'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives',
+#                  'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius',
+#                  'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia',
+#                  'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands',
+#                  'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Macedonia', 'Norway', 'Oman', 'Pakistan',
+#                  'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal',
+#                  'Qatar', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia',
+#                  'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia',
+#                  'Senegal', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia',
+#                  'Solomon Islands', 'Somalia', 'South Africa', 'Spain', 'Sri Lanka', 'Sudan',
+#                  'Suriname', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China',
+#                  'Tajikistan', 'Tanzania, United Republic of', 'Thailand',
+#                  'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda',
+#                  'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan',
+#                  'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Yemen', 'Zambia',
+#                  'Zimbabwe']
+#
+# hazard_list = ['relative_crop_yield']  # ['tropical_cyclone', 'river_flood', 'storm_europe', 'relative_crop_yield']
+# sector_list = ['agriculture']  # 'mining', 'manufacturing', 'service', 'electricity', 'agriculture'
+#
+# scenario = 'historical'  # 'rcp60', 'rcp26', 'rcp45','None', 'historical'
+# ref_year = '1971_2001'  # 'historical', 2040, 2060, 2080, 2020 #2020 works for river_flood only
+# n_sim_years = 100
+# io_approach = 'ghosh'
 
+config = {
+    "io_approach": "ghosh",
+    "n_sim_years": 100,
+    "runs": [
+        {
+            "hazard": "tropical_cyclone",
+            "sectors": ["agriculture"],
+            "countries": ["United States"],
+            "scenario_years": [
+                {"scenario": "None", "ref_year": "historical"},
+                {"scenario": "rcp26", "ref_year": "2040"},
+            ]
+        },
+        {
+            "hazard": "winterstorm",
+            "sectors": ["agriculture"],
+            "countries": ["United States"],
+            "scenario_years": [
+                {"scenario": "ssp126", "ref_year": "historical"},
+                {"scenario": "rcp26", "ref_year": "2040"},
+            ]
+        }
+    ]
+}
 
-
-#Check for country names with this website: https://github.com/flyingcircusio/pycountry/blob/main/src/pycountry/databases/iso3166-1.json
-
-country_list = ['United States']
-ALL_COUNTRIES = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina',
-                  'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados',
-                  'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia, Plurinational State of',
-                  'Bosnia and Herzegovina', 'Botswana','Brazil', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso',
-                    'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon',
-                  'Canada', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo',
-                  'Congo, The Democratic Republic of the', 'Costa Rica', 'Croatia',"Côte d'Ivoire",
-                  'Cuba', 'Cyprus', 'Czechia', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic',
-                  'Timor-Leste', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea',
-                  'Estonia', 'Eswatini', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia',
-                  'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti',
-                  'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq',
-                  'Ireland','Israel','Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati',
-                  "Korea, Democratic People's Republic of",
-                  'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia', 'Lebanon',
-                  'Lesotho', 'Liberia',
-                  'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives',
-                  'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius',
-                  'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia',
-                  'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands',
-                  'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Macedonia', 'Norway', 'Oman', 'Pakistan',
-                  'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal',
-                  'Qatar', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia',
-                  'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia',
-                  'Senegal', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia',
-                  'Solomon Islands', 'Somalia', 'South Africa', 'Spain', 'Sri Lanka', 'Sudan',
-                  'Suriname', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China',
-                  'Tajikistan','Tanzania, United Republic of', 'Thailand',
-                  'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda',
-                  'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan',
-                  'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Yemen', 'Zambia',
-                  'Zimbabwe']
-
-hazard_list = ['relative_crop_yield']  # ['tropical_cyclone', 'river_flood', 'storm_europe', 'relative_crop_yield']
-sector_list = ['agriculture'] # 'mining', 'manufacturing', 'service', 'electricity', 'agriculture'
-
-scenario = 'historical' # 'rcp60', 'rcp26', 'rcp45','None', 'historical'
-ref_year = '1971_2001' # 'historical', 2040, 2060, 2080, 2020 #2020 works for river_flood only
-n_sim_years = 100
-io_approach = 'ghosh'
 
 # Agriculture
 # 2006_2099, 1976_2005
@@ -135,6 +162,7 @@ def calc_supply_chain_impacts(
         scenario,
         ref_year,
         n_sim_years,
+        io_approach,
         save_by_country=False,
         save_by_hazard=False,
         save_by_sector=False,
@@ -192,14 +220,12 @@ def calc_supply_chain_impacts(
     print("Done!\nTo show the Dashboard run:\nbokeh serve dashboard.py --show")
 
 
-if __name__ == "__main__":
-    #Added a loop to run for each country to have intermediate files
-
+def run_pipeline(country_list, hazard, sector_list, scenario, ref_year, n_sim_years, io_approach):
     for country in country_list:
         try:
             calc_supply_chain_impacts(
                 [country],  # replace by country_list if whole list should be calculated at once
-                hazard_list,
+                [hazard],
                 sector_list,
                 scenario,
                 ref_year,
@@ -208,8 +234,6 @@ if __name__ == "__main__":
             )
         except Exception as e:
             print(f"Could not calculate country {country} {sector_list} due to {e}")
-
-
 
     # Postprocessing to create the final files
     supchain = indirect.get_supply_chain()
@@ -227,3 +251,21 @@ if __name__ == "__main__":
         print(f"Adjusting {f} by {factor} to {f_out}")
         df.to_csv(f_out, index=False)
     print("Done!\nTo show the Dashboard run:\nbokeh serve dashboard.py --show")
+
+
+def run_pipeline_from_config(config):
+    for run in config["runs"]:
+        for scenario_year in run["scenario_years"]:
+            run_pipeline(
+                run["countries"],
+                run["hazard"],
+                run["sectors"],
+                scenario_year["scenario"],
+                scenario_year["ref_year"],
+                config["n_sim_years"],
+                config["io_approach"]
+            )
+
+
+if __name__ == "__main__":
+    run_pipeline_from_config(config)
