a6a6cecee23778130ffc0b0fc42b53694c38035d
Gaudenz Halter <gaudenz.halter@live.com>
2024-01-22 10:46:33 +0100

    .refactor(pipeline): just some small changes

--- pipeline/indirect/indirect.py
+++ pipeline/indirect/indirect.py
@@ -45,8 +45,10 @@ def get_secs_prod(supchain: SupplyChain, country_iso3alpha, impacted_secs, n_tot
     """
     mrio_region = supchain.map_exp_to_mriot(country_iso3alpha, "WIOD16")
     if mrio_region == 'ROW':
-        return (1 / (n_total - (len(set(r[0] for r in supchain.mriot.x.axes[0])) - 1)))*supchain.mriot.x.loc[("ROW", impacted_secs), :]
+        return (1 / (n_total - (len(set(r[0] for r in supchain.mriot.x.axes[0])) - 1))) * supchain.mriot.x.loc[
+                                                                                          ("ROW", impacted_secs), :]
     return supchain.mriot.x.loc[(country_iso3alpha, impacted_secs), :]
+
 
 def get_secs_shock(supchain: SupplyChain, country_iso3alpha, impacted_secs, n_total=195):
     """
@@ -69,18 +71,19 @@ def get_secs_shock(supchain: SupplyChain, country_iso3alpha, impacted_secs, n_to
         for each event with impacts. Columns are the same as the chosen MRIOT and rows are the
         hazard events ids.
     """
-    #TODO check if which one should be used: secs_shock or secs_imp (see definitions above)
+    # TODO check if which one should be used: secs_shock or secs_imp (see definitions above)
     # I would argue to still use secs_shock as it accounts for the exposure impact ratio (The attribute
     # self.secs_shock is proportional to the ratio between self.secs_imp and self.secs_exp, so self.secs_shock
     # is a number between 0 and 1. self.secs_shock will be used in the indirect impact calculation to assses
     # how much production loss is experienced by each sector.)
     # if using secs_shock again, the value extractions would need to change again
 
-
     mrio_region = supchain.map_exp_to_mriot(country_iso3alpha, "WIOD16")
     if mrio_region == 'ROW':
-        return (1 / (n_total - (len(set(r[0] for r in supchain.mriot.x.axes[0])) - 1)))*supchain.secs_shock.loc[:, ("ROW", impacted_secs)] #TODO secs_shock vs secs_imp
-    return supchain.secs_shock.loc[:, (country_iso3alpha, impacted_secs)] #TODO secs_shock vs secs_imp
+        row_fract_per_county = 1 / (n_total - (len(set(r[0] for r in supchain.mriot.x.axes[0])) - 1))
+        return row_fract_per_county * supchain.secs_shock.loc[:, (impacted_secs, "ROW")]  # TODO secs_shock vs secs_imp
+    return supchain.secs_shock.loc[:, (country_iso3alpha, impacted_secs)]  # TODO secs_shock vs secs_imp
+
 
 def get_supply_chain() -> SupplyChain:
     return SupplyChain.from_mriot(mriot_type='WIOD16', mriot_year=2011)
@@ -128,33 +131,41 @@ def dump_direct_to_csv(supchain,
     sec_range = SUPER_SEC[sector]
     impacted_secs = supchain.mriot.get_sectors()[sec_range].tolist()
     country_iso3alpha = pycountry.countries.get(name=country).alpha_3
-    # calls the function which is especially important for ROW countries as the country code does not exist
+
+    # Get the total production of each subsector in the impacted sector, in the case of a ROW country, the total
+    # production is divided by the number of countries in the mrio table
     secs_prod = get_secs_prod(supchain, country_iso3alpha, impacted_secs)
+
     # create a lookup table for each sector and its total production
     lookup = {}
     for idx, row in secs_prod.iterrows():
         lookup[idx] = row["total production"]
 
     for (sec, v) in get_secs_shock(supchain, country_iso3alpha, impacted_secs).items():
-        # TODO What to do about the exposure units, Litpop exposure is in USD, is the direct impact then also in USD?
-        # TODO If using secs_shock, we would need to divide the impacts by 1 Million, as the total production of the mrio is in MEUR
-        rp_value = v.sort_values(ascending=False).iloc[index_rp]  # TODO cehck if / 1000000  is required
-        mean_ratio = (v.sum() / n_sim)  # TODO cehck if / 1000000  is required
-        max_val = v.max()  # TODO cehck if / 1000000  is required
+        # NOTE we are using the SHOCK TABLE instead of the  IMPACT_TABLE. The shock table tells us what fraction of
+        # the sector is impacted. (impacted asset value / total asset value). If we'd use the impact table, we
+        # would have to convert the currencies and units of the exposure and the mrio table to match.
+
+        # First we extract the values from the shock table, these are only ratios, not the actual production loss
+        # A 100rp ratio of outage
+        rp_ratio = v.sort_values(ascending=False).iloc[index_rp]
+        # A average annual outage ratio
+        avg_ann_ratio = (v.sum() / n_sim)
+        # The maximum outage ratio
+        max_ratio = v.max()
 
         # Check if the denominator is non-zero before performing division
-
         total_production = lookup[sec]
         obj = {
             "sector": sec[1],
             "total_sectorial_production_mriot": lookup[sec],
 
-            "maxPL": max_val * total_production, # TODO: check if this is correct, using secs_shock would only require max_val
-            "rmaxPL": max_val * 100, # TODO: check if this correct, secs_shock would need (max_val / total_production)*100
-            "AAPL": mean_ratio * total_production, # TODO: check if this is correct, secs_shock would only require mean_ratio
-            "rAAPL": mean_ratio * 100, # TODO: check if this is correct, secs_shock wpuld require: (mean_ratio / total_production)*100
-            f"PL{return_period}": rp_value * total_production,  # TODO: check if this is correct,
-            f"rPL{return_period}": rp_value * 100,  # TODO: check if this is correct,
+            "maxPL": max_ratio * total_production,
+            "rmaxPL": max_ratio * 100,
+            "AAPL": avg_ann_ratio * total_production,
+            "rAAPL": avg_ann_ratio * 100,
+            f"PL{return_period}": rp_ratio * total_production,
+            f"rPL{return_period}": rp_ratio * 100,
             "hazard_type": haz_type,
             "sector_of_impact": sector,
             "scenario": scenario,
@@ -206,17 +217,15 @@ def dump_supchain_to_csv(supchain,
         lookup[idx] = row["total production"]
 
     for (sec, v) in supchain.supchain_imp[f"{io_approach}"].loc[:, ('CHE', slice(None))].items():
-
         # We scale all values such that countries in the rest of the world category
         # are divided evenly by the number of countries in ROW. Countries explicitely in the MRIO
         # table have a rotw_factor of 1
-        #TODO check again the units of the output, does the exposure value unit match the mriot value unit?
+        # TODO check again the units of the output, does the exposure value unit match the mriot value unit?
         rp_value = v.sort_values(ascending=False).iloc[index_rp] * rotw_factor
         mean = (v.sum() / n_sim) * rotw_factor
         max_val = v.max() * rotw_factor
 
-
-        total_production = lookup[sec[1]]    #no muliply with the rotw fctor, since we use the Swiss productions
+        total_production = lookup[sec[1]]  # no multiply with the rotw factor, since we use the Swiss productions
         obj = {
             "sector": sec[1],
             "total_sectorial_production_mriot_CHE": total_production,
