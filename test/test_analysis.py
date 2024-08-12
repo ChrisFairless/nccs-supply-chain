0d82edc9a7625b2ab8dbba3228383c16b40d6d87
Chris Fairless <chrisfairless@hotmail.com>
2024-08-12 15:10:15 +0200

    v1 of data output validations

--- test/test_analysis.py
+++ test/test_analysis.py
@@ -7,14 +7,19 @@ from analysis import run_pipeline_from_config
 from run_configurations.test.test_config import CONFIG  # change here to test_config if needed
 from utils.folder_naming import get_direct_output_dir
 from utils.delete_results import delete_results_folder
+from utils.data_validation import validate_from_config
 
 class TestAnalysisPipeline(unittest.TestCase):
     
     # Simplest possible test: check it runs without errors
     # TODO: less simple possible tests 
+
     def test_pipeline_runs(self):
         delete_results_folder(CONFIG['run_title'])
         _ = run_pipeline_from_config(CONFIG)
+    
+    def test_data_validation(self):
+        validation_df = validate_from_config(CONFIG)
 
 
 if __name__ == '__main__':
