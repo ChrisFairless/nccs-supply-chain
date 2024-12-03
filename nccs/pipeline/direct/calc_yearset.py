c2f526277fc5eaa14f95e64f972743ebfacd5fa2
Chris Fairless <chrisfairless@hotmail.com>
2024-12-03 18:05:20 +0100

    Remove unused method. This wasn't the fix I thought it was

--- nccs/pipeline/direct/calc_yearset.py
+++ nccs/pipeline/direct/calc_yearset.py
@@ -8,57 +8,13 @@ from climada.entity import Exposures
 from climada.engine import Impact, ImpactCalc
 from climada.util import yearsets
 
-
-# THIS IS A QUICK FIX FOR A MORE COMPLEX PROBLEM
-# CLIMADA's yearsets currently generate years of data through a Poisson process. That is, the number of 'events' in a 
-# year is a random variable. This is great when our events are e.g. tropical cyclones and we're sampling from a 
-# stochastic set. It doesn't work when our events are e.g. wildfire or windstorm years. In this case we always want to 
-# sample exactly one 'event' per year. At some point we can add this functionality to CLIMADA as an additional method
-# within the yearsets code, but for now I'm making a workaround where if the Poisson process is asked to sample events 
-# with a frequency of 1/year it doesn't implement a Poisson process and instead returns exactly one event per year.
-# This isn't a valid fix because it's plausible that a user would eventually ask for a Poisson process with lamda = 1/yr 
-# and wouldn't be able to get that.
-def sample_from_poisson(n_sampled_years, lam, seed=None):
-    """Sample the number of events for n_sampled_years
-
-    Parameters
-    -----------
-        n_sampled_years : int
-            The target number of years the impact yearset shall contain.
-        lam: int
-            the applied Poisson distribution is centered around lambda events per year
-        seed : int, optional
-            seed for numpy.random, will be set if not None
-            default: None
-
-    Returns
-    -------
-        events_per_year : np.ndarray
-            Number of events per sampled year
-    """
-    if seed is not None:
-        np.random.seed(seed)
-    if lam != 1:
-        events_per_year = np.round(
-            np.random.poisson(
-                lam=lam,
-                size=n_sampled_years
-            )
-        ).astype('int')
-    else:
-        events_per_year = np.ones(n_sampled_years).astype('int')
-
-    return events_per_year
-
-
-yearsets.sample_from_poisson = sample_from_poisson
 LOGGER = logging.getLogger(__name__)
 
 
 def yearset_from_imp(imp, n_sim_years, poisson=True, cap_exposure=None, seed=None):
     if poisson:
         lam = np.sum(imp.frequency)
-        LOGGER.info('Correcting TC event frequencies: once these have been update by Samuel this can be removed')
+        LOGGER.info('Correcting TC event frequencies: once these have been updated by Samuel this can be removed')
         lam = lam * 25 / 26
         yimp, samp_vec = yearsets.impact_yearset(
             imp,
