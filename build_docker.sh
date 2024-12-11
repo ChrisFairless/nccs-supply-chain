4bcf34a8256788b0c462ac4bea1291616645fd5a
Gaudenz Halter <gaudenz.halter@live.com>
2024-12-11 16:38:00 +0100

    .wip(dashboard): updated dashboard

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
-docker build . -f  "Dockerfile" -t correnticscontainers.azurecr.io/nccs:v1.15.0 --label dockerfile-path=Dockerfile
-docker push correnticscontainers.azurecr.io/nccs:v1.15.0
+docker build . -f  "Dockerfile" -t correnticscontainers.azurecr.io/nccs:v2.0.0 --label dockerfile-path=Dockerfile
+docker push correnticscontainers.azurecr.io/nccs:v2.0.0
 
 kubectl apply -f deployment.yml
